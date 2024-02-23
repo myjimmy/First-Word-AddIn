3453f07ad70e8f761bacf5022e3283aeccc4ba14
Jimmy <myjimmy@protonmail.com>
2024-02-22 19:14:43 -0800

    ADD-IN: Added code to insert the constant text.

--- FirstWordAddIn/ThisAddIn.cs
+++ FirstWordAddIn/ThisAddIn.cs
@@ -32,5 +32,11 @@ namespace FirstWordAddIn
         }
         
         #endregion
+
+        void Application_DocumentBeforeSave(Word.Document Doc, ref bool SaveAsUI, ref bool Cancel)
+        {
+            Doc.Paragraphs[1].Range.InsertParagraphBefore();
+            Doc.Paragraphs[1].Range.Text = "This text was added by using code.";
+        }
     }
 }
