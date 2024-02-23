d2374a4eda6faf9be209fdadf82c7038b8087503
Jimmy <myjimmy@protonmail.com>
2024-02-22 23:21:09 -0800

    CUSTOM-RIBBON-BAR: Added the custom ribbon tab.

---
+++ FirstWordAddIn/MyRibbon.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FirstWordAddIn
+{
+    partial class MyRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MyRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.btnAbout = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // tab2
+            // 
+            this.tab2.Groups.Add(this.group2);
+            this.tab2.Label = "MyTab";
+            this.tab2.Name = "tab2";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.btnAbout);
+            this.group2.Label = "Info";
+            this.group2.Name = "group2";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnAbout.ImageName = "About";
+            this.btnAbout.Label = "button1";
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.ShowImage = true;
+            this.btnAbout.SuperTip = "Information about this add-in.";
+            this.btnAbout.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.onAction_button);
+            // 
+            // MyRibbon
+            // 
+            this.Name = "MyRibbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MyRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnAbout;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MyRibbon Ribbon1
+        {
+            get { return this.GetRibbon<MyRibbon>(); }
+        }
+    }
+}
---
+++ FirstWordAddIn/MyRibbon.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstWordAddIn
+{
+    public partial class MyRibbon
+    {
+        private void MyRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void onAction_button(object sender, RibbonControlEventArgs e)
+        {
+            switch (e.Control.Id)
+            {
+                case "btnAbout":
+                    break;
+                default: break;
+            }
+        }
+    }
+}
